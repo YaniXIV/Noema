8a8e974b14a3731ec13cb1578a6b293fe2629908
Yani <Yanixiv.dev@gmail.com>
2026-02-02 17:48:44 -0700

    gemini chat

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
-	s "noema/server"
+	"github.com/joho/godotenv"
+	//s "noema/server"
+	"context"
+	"google.golang.org/genai"
+	"log"
 )
 
+func initVariables() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func makeQuery(s string) {
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result, err := client.Models.GenerateContent(
+		ctx,
+		"gemini-3-flash-preview",
+		genai.Text(s),
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result.Text())
+
+}
+
 func main() {
-	fmt.Println("Hello World")
-	s.InitServer()
+	initVariables()
+	var userin string
+	for {
+		fmt.Scanln(&userin)
+		makeQuery(userin)
+	}
 }
